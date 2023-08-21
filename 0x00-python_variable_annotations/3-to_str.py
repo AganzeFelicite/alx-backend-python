fb6f53c7942cdc5ee5dd98c73857e3ca6a6ba849
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-21 20:01:57 +0200

    this is to_string module

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+this is a module that returns a
+string representation of a the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    a function that returns a
+    string representation of
+    float
+    """
+    return "".format(n)
