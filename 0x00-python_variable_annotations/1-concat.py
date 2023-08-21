6449d63abc451ce90aa39eb3ff082ac6526a5b3e
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-21 18:51:11 +0200

    string concatination in static type

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+this is a module to concatinate
+two string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    parameter
+    str1 : a string
+    str2 : a string
+    """
+    return str1 + str2
