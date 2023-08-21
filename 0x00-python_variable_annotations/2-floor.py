73a5a72cacacbb0e640bb5f86336a091cc4e2853
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-21 19:06:36 +0200

    floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+this is a module that takes in a float
+and return its flooor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    it takes in one parameter a float
+    and returns its floor value
+    """
+    return int(n)
