2fded5d59f5614ce957ac2a363c8d6bb467212a7
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-21 18:38:46 +0200

    initial commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+this module adds two numbers
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    parameters
+        a : float
+        b : float
+    """
+    return a + b
