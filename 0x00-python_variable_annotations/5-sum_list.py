3f874bff2de74596e7949c67a8d856c1d4cf3cee
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-21 20:27:09 +0200

    arrays

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+takes in a list of floats
+and returns their
+sum
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    this function returns
+    a sum of values in the list
+    """
+    return sum(input_list)
