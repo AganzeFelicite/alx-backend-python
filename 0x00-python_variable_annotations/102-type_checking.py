ac934bab4295f25c0b0fb7c134d95ab3956dcf2e
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-22 15:30:48 +0200

    done with advanced task

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+this is a module on how to
+use mypy to debug types in
+python
+"""
+
+
+from typing import Tuple, List, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
