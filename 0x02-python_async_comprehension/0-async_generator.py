1a6beb8fc09f4d772cd061c481b83a27b4242f25
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-23 21:10:42 +0200

    hot fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,12 +6,12 @@ generator
 """
 
 
-from typing import AsyncGenerator
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, Non]:
     """
     generators in python
     and asyncio
