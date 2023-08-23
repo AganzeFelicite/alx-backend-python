1b239a53194949868e7e5692d7e88f0a6b9bea6f
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-23 15:15:57 +0200

    typo fixer

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -26,7 +26,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     for i in range(n - 1):
         for j in range(n-1-i):
             if (arr[j] > arr[j + 1]):
-                temp: float = arr[j]
+                temp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
 
